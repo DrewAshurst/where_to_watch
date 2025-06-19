bf767fafe2a242192e9ee2a48d83e72c3cc7f9a8
Drew-Ashurst <drewashurst97@gmail.com>
2025-06-18 23:41:27 -0400

    cleaned up code

--- classes/gui.py
+++ classes/gui.py
@@ -3,48 +3,97 @@ from .data import Data
 
 
 class Gui:
-    def __init__(self):
-        self.api = Data()
+    def __init__(self, config):
+        self.config = config
+        self.api = Data(config)
         self.root = tk.Tk()
 
         self.root.geometry("400x400")
+        self.output_count = self.config["output_count"]
         self.main_page()
 
-    def clear_window(self):
-        for widget in self.root.winfo_children():
+    def clear_window(self, frame):
+        for widget in frame.winfo_children():
             widget.destroy()
+
+    def get_checkbox_values(self, checkboxes):
+        accepted = {
+            'option': [],
+            'service': []
+        }
+        for key, val in checkboxes.items():
+            if key.lower() in ['free', 'subscription', 'buy'] and val[1].get():
+                accepted['option'].append(key.lower())
+            elif val[1].get():
+                accepted['service'].append(key)
+
+        return accepted
+
 
     def title_search(self, textbox, type, checkboxes):
         value = textbox.get("1.0", tk.END).strip()
-        for key, val in checkboxes.items():
-            print(key, val[1].get())
-        return_data = self.api.search_content(value, type, checkboxes, True)
-        self.title_search_page(True, return_data, checkboxes)
+        self.get_checkbox_values(checkboxes)
+        accepted = self.get_checkbox_values(checkboxes)
 
-    def select_all_boxes(self, checkboxes):
+        return_data = self.api.search_content(value, type, accepted, True)
+        self.show_output(accepted, return_data, type)
+
+    def show_output(self, accepted, df, type):
+        self.clear_window(self.output_frame)
+        cols = ['show_key', 'genres',  'rating']
+
+        if type == 'series':
+            cols = cols + ['season_count', 'episode_count'] + accepted['option']
+        else:
+            cols = cols + ['runtime'] + accepted['option']
+        output = df[cols]
+        label_container = tk.Frame(self.output_frame, bg="deep sky blue")
+        label_container.pack(anchor='center')
+        
+
+        for row in output.values.tolist()[:self.output_count]:
+            out_string = ''
+            for i in range(len(row)):
+                print('col', cols[i])
+                if cols[i] in ['free', 'subscription', 'buy'] and row[i]:
+                    for val in row[i].split('|'):
+                        out_string += f'{val}: {cols[i].capitalize()}\n'
+
+                elif cols[i] == 'show_key':
+                    vals = row[i].split('|')
+                    out_string += f'{vals[0]}: {vals[1]}\n'
+                elif row[i]:
+                    out_string += f'{cols[i].replace("_", " ").capitalize()}: {row[i]}\n'
+
+            tk.Label(
+                label_container,
+                relief="raised",
+                borderwidth=4,
+                text=out_string,
+                font=("Helvetica", 14),
+                fg="black",
+                bg="goldenrod",
+            ).pack(padx=15, side="left")
+            
+
+
+    def all_box_action(self, checkboxes, value):
         for key, box in checkboxes.items():
             if key in self.api.watch_options:
                 continue
-            box[1].set(True)
-
-    def deselect_all_boxes(self, checkboxes):
-        for key, box in checkboxes.items():
-            if key in self.api.watch_options:
-                continue
-            box[1].set(False)
+            box[1].set(value)
 
     def title_search_page(self, show_output=False, output=None, old_checkboxes=None):
-        self.clear_window()
+        self.clear_window(self.root)
 
         service_list = sorted(self.api.config["services"])
 
         # main frame which houses all others
         main_frame = tk.Frame(self.root, bg="deep sky blue", borderwidth=2)
-
         textbox_frame = tk.Frame(main_frame, bg="deep sky blue", borderwidth=2)
         button_frame = tk.Frame(main_frame, bg="deep sky blue", borderwidth=2)
         checkbox_frame = tk.Frame(main_frame, bg="deep sky blue", borderwidth=2)
-        output_frame = tk.Frame(main_frame, bg="deep sky blue", borderwidth=2)
+        self.output_frame = tk.Frame(main_frame, bg="deep sky blue", borderwidth=2)
 
         # columns for checkbox layout
         cb_cols = [
@@ -72,57 +121,25 @@ class Gui:
         )
 
         checkboxes = {}
-        check_count = 0
-        for service in service_list:
+        check_box_column_count = 5
+        col_ind = 0
+
+        for i in range(len(service_list)):
             var = tk.BooleanVar()
-            if check_count < 5:
-                checkboxes[service] = [
+            if i >= col_ind * check_box_column_count and i < (col_ind+1) * check_box_column_count:
+                checkboxes[service_list[i]] = [
                     tk.Checkbutton(
-                        cb_cols[0],
-                        text=service,
+                        cb_cols[col_ind],
+                        text=service_list[i],
                         variable=var,
                         bg="goldenrod",
                         relief="groove",
                     ),
                     var,
                 ]
-            elif check_count >= 5 and check_count < 10:
-                checkboxes[service] = [
-                    tk.Checkbutton(
-                        cb_cols[1],
-                        text=service,
-                        variable=var,
-                        bg="goldenrod",
-                        relief="groove",
-                    ),
-                    var,
-                ]
-            elif check_count >= 10 and check_count < 15:
-                checkboxes[service] = [
-                    tk.Checkbutton(
-                        cb_cols[2],
-                        text=service,
-                        variable=var,
-                        bg="goldenrod",
-                        relief="groove",
-                    ),
-                    var,
-                ]
-            else:
-                checkboxes[service] = [
-                    tk.Checkbutton(
-                        cb_cols[3],
-                        text=service,
-                        variable=var,
-                        bg="goldenrod",
-                        relief="groove",
-                    ),
-                    var,
-                ]
-            if old_checkboxes:
-                if old_checkboxes[service][1].get():
-                    checkboxes[service][1].set(True)
-            check_count += 1
+            if i == ((col_ind+1) * check_box_column_count) - 1:
+                col_ind += 1
+
 
         for option in self.api.watch_options:
             var = tk.BooleanVar()
@@ -137,16 +154,12 @@ class Gui:
                 ),
                 var,
             ]
-            if old_checkboxes:
-                if old_checkboxes[option][1].get():
-                    checkboxes[option][1].set(True)
-            check_count += 1
 
         main_frame.pack(padx=20, pady=20, fill="both", expand=True, anchor="n")
         textbox_frame.pack(padx=20, pady=20, anchor="n")
         button_frame.pack(padx=20, pady=20, anchor="n")
         checkbox_frame.pack(padx=20, pady=20, fill="both", anchor="n", expand=True)
-        output_frame.pack(padx=20, pady=20, fill="both", anchor="n", expand=True)
+        self.output_frame.pack(padx=20, pady=20, fill="both", anchor="n", expand=True)
 
         textbox.pack(pady=10, anchor="n")
         dropdown.pack(pady=10)
@@ -163,7 +176,7 @@ class Gui:
             width=20,
             height=3,
             text="Select all Services",
-            command=lambda: self.select_all_boxes(checkboxes),
+            command=lambda: self.all_box_action(checkboxes, True),
             bg="goldenrod",
         )
         none_button = tk.Button(
@@ -171,39 +184,15 @@ class Gui:
             width=20,
             height=3,
             text="Deselect all Services",
-            command=lambda: self.deselect_all_boxes(checkboxes),
+            command=lambda: self.all_box_action(checkboxes, False),
             bg="goldenrod",
         )
 
         all_button.pack()
         none_button.pack()
 
-        if output:
-            for key, val in output.items():
-                print(val)
-                text_val = " - ".join(key.split("|"))
-                text_val += "\nStreaming Options:\n"
-                for sub_key, sub_val in val["services"].items():
-                    if sub_val:
-                        text_val += f"{sub_key}: {sub_val.capitalize()}\n"
-
-                for sub_key, sub_val in val["details"].items():
-                    text_val += f"{sub_key.capitalize()}: {sub_val}\n"
-
-                text_val += f'Genres: {", ".join(val["genres"].split("|"))}'
-
-                tk.Label(
-                    output_frame,
-                    relief="raised",
-                    borderwidth=4,
-                    text=text_val,
-                    font=("Helvetica", 14),
-                    fg="black",
-                    bg="goldenrod",
-                ).pack(padx=15, side="left", anchor="nw")
-
     def main_page(self):
-        self.clear_window()
+        self.clear_window(self.root)
 
         button_frame = tk.Frame(self.root, bg="deep sky blue", borderwidth=2)
         button = tk.Button(
